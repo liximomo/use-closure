dfa3548bb17e5d0ed290038cfb54475a93f36ead
liximomo <liximomo@gmail.com>
2019-02-27 17:23:09 +0800

    fix: compatible with react-dev-tool

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,22 +1,38 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import camelCase from 'lodash.camelcase';
+import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
 
-const pkg = require('./package.json')
+const pkg = require('./package.json');
 
-const libraryName = 'use-closure'
+const libraryName = 'use-closure';
 
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    {
+      file: pkg.main,
+      name: camelCase(libraryName),
+      format: 'umd',
+      sourcemap: true,
+      globals: {
+        react: 'React',
+      },
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+      globals: {
+        react: 'React',
+      },
+    },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: ['react'],
+
   watch: {
     include: 'src/**',
   },
@@ -26,7 +42,12 @@ export default {
     // Compile TypeScript files
     typescript({ useTsconfigDeclarationDir: true }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'node_modules/react/index.js': ['useRef', 'useEffect'],
+      },
+    }),
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
@@ -35,4 +56,4 @@ export default {
     // Resolve source maps to the original source
     sourceMaps(),
   ],
-}
+};
--- src/use-closure.ts
+++ src/use-closure.ts
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useCallback, useState } from 'react';
+import { useRef, useEffect } from 'react';
 
 type Ref<T> = React.MutableRefObject<T | undefined>;
 type Effect = React.EffectCallback;
@@ -9,98 +9,33 @@ interface ClosureWrapper<T> {
 }
 
 interface CaptureState<T> {
-  renderEffect: Effect;
-  wokrInProgressFns: T[];
-  closures: ClosureWrapper<T>[];
-  cursor: number;
-}
-
-interface SimpleCaptureState<T> {
-  renderEffect: Effect;
+  captureEffect: Effect;
   wokrInProgressFn?: T;
-  closure?: ClosureWrapper<T>;
+  closure: ClosureWrapper<T>;
 }
 
-type Capture<T> = React.MutableRefObject<CaptureState<T>>;
-type SimpleCapture<T> = React.MutableRefObject<SimpleCaptureState<T>>;
-
-function useSimpleCapture<T>(): SimpleCapture<T> {
-  const capture = useRef<SimpleCaptureState<T>>();
-
-  if (!capture.current) {
-    const captureState: SimpleCaptureState<T> = {
-      renderEffect() {
-        captureState.closure!.ref.current = captureState.wokrInProgressFn;
+export default function useClosure<T extends Function>(fn: T): T {
+  const capture = useRef<CaptureState<T>>();
+  let captureState = capture.current;
+  if (!captureState) {
+    captureState = {
+      captureEffect() {
+        ref.current = captureState!.wokrInProgressFn;
       },
-    };
-    capture.current = captureState;
-  }
-
-  return capture as SimpleCapture<T>;
-}
-
-export function useCapture(): Capture<any> {
-  const capture = useRef<CaptureState<any>>();
-
-  if (!capture.current) {
-    const captureState: CaptureState<any> = {
-      renderEffect() {
-        captureState.wokrInProgressFns.forEach((fn, index) => {
-          captureState.closures[index].ref.current = fn;
-        });
-        captureState.cursor = -1;
-      },
-      wokrInProgressFns: [],
-      closures: [],
-      cursor: -1,
-    };
-    capture.current = captureState;
-  }
-
-  return capture as Capture<any>;
-}
-
-export default function useClosure<T extends Function>(fn: T, capture?: Capture<T>): T {
-  if (capture) {
-    const captureState = capture.current;
-    captureState.cursor = captureState.cursor + 1;
-
-    // first enter
-    if (captureState.cursor === 0) {
-      useEffect(captureState.renderEffect);
-    }
-
-    const cursor = captureState.cursor;
-    let closure = captureState.closures[cursor];
-    if (!closure) {
-      closure = {
-        proxy: (((...args: any[]) => closure.ref.current!(...args)) as any) as T,
+      closure: {
+        proxy: (((...args: any[]) => ref.current!(...args)) as any) as T,
         ref: {
           current: undefined,
         },
-      };
-      captureState.closures[cursor] = closure;
-    }
-    captureState.wokrInProgressFns[cursor] = fn;
-
-    return closure.proxy;
-  }
-
-  const simpleCapture = useSimpleCapture<T>();
-  const captureState = simpleCapture.current;
-  let closure = captureState.closure;
-  if (!closure) {
-    closure = {
-      proxy: (((...args: any[]) => closure!.ref.current!(...args)) as any) as T,
-      ref: {
-        current: undefined,
       },
     };
-    captureState.closure = closure;
+    // store ref for quickly access
+    const ref = captureState.closure.ref;
+    capture.current = captureState;
   }
   captureState.wokrInProgressFn = fn;
 
-  useEffect(captureState.renderEffect);
+  useEffect(captureState.captureEffect);
 
-  return closure.proxy;
+  return captureState.closure.proxy;
 }
