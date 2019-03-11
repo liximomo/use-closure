867283b286a8cad9c16e4705c704a6f87edef621
liximomo <liximomo@gmail.com>
2019-03-11 23:39:11 +0800

    simplify code

--- src/use-closure.ts
+++ src/use-closure.ts
@@ -3,15 +3,15 @@ import { useRef, useEffect } from 'react';
 type Ref<T> = React.MutableRefObject<T | undefined>;
 type Effect = React.EffectCallback;
 
-interface ClosureWrapper<T> {
+interface ProxyFunction<T> {
   proxy: T;
-  ref: Ref<T>;
+  target: T;
 }
 
 interface CaptureState<T> {
   captureEffect: Effect;
-  wokrInProgressFn?: T;
-  closure: ClosureWrapper<T>;
+  wokrInProgressFn: T;
+  closure: ProxyFunction<T>;
 }
 
 export default function useClosure<T extends Function>(fn: T): T {
@@ -20,20 +20,20 @@ export default function useClosure<T extends Function>(fn: T): T {
   if (!captureState) {
     captureState = {
       captureEffect() {
-        ref.current = captureState!.wokrInProgressFn;
+        funcProxy.target = captureState!.wokrInProgressFn;
       },
+      wokrInProgressFn: fn,
       closure: {
-        proxy: (((...args: any[]) => ref.current!(...args)) as any) as T,
-        ref: {
-          current: undefined,
-        },
+        proxy: (((...args: any[]) => funcProxy.target(...args)) as any) as T,
+        target: fn,
       },
     };
     // store ref for quickly access
-    const ref = captureState.closure.ref;
+    const funcProxy = captureState.closure;
     capture.current = captureState;
+  } else {
+    captureState.wokrInProgressFn = fn;
   }
-  captureState.wokrInProgressFn = fn;
 
   useEffect(captureState.captureEffect);
 
